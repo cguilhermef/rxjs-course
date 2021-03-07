7be4e264e9aa0c33b58edb10053f53aec191285c
Christian G. Fritsch <christian@checklistfacil.com.br>
2021-03-07 11:42:11 -0300

    feat: created code from first lesson to 11th lesson

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
 import {interval, Observable, of, timer} from 'rxjs';
 import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -11,14 +12,26 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 })
 export class HomeComponent implements OnInit {
 
-
-    constructor() {
-
-    }
+    beginnerCourses$: Observable<Course[]>;
+    advancedCourses$: Observable<Course[]>;
 
     ngOnInit() {
 
+        const http$ = createHttpObservable('/api/courses');
 
+        const courses$: Observable<Course[]> = http$.pipe(
+            tap(() => console.log('HTTP request executed')),
+            map((response) => Object.values(response['payload'])),
+            shareReplay<Course[]>()
+        );
+
+        this.beginnerCourses$ = courses$.pipe(
+            map((courses: Course[]) => courses.filter((course) => course.category === 'BEGINNER'))
+        );
+
+        this.beginnerCourses$ = courses$.pipe(
+            map((courses: Course[]) => courses.filter((course) => course.category === 'BEGINNER'))
+        );
 
     }
 
